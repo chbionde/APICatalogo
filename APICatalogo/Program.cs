d0b53d2775deaddf08693b9d4f7acf36105ddcd7
Carlos Bionde <bionde.trab@gmail.com>
2025-07-04 20:16:11 -0300

    updating and creating db using migration on dotnet-ef

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -1,15 +1,19 @@
+using APICatalogo.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(connectionString, 
+    ServerVersion.AutoDetect(connectionString)));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
