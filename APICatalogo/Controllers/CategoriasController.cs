2a61a26ac2d826621defe8a7325e6a394949f619
Carlos Bionde <bionde.trab@gmail.com>
2025-07-07 20:26:50 -0300

    feat#51: created controller 'categorias' and optimizing

---
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,89 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public CategoriasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("produtos")]
+        public ActionResult<IEnumerable<Categoria>> GetCategoriasProdutos()
+        {
+            var categoriasProdutos = _context.Categorias.Include(p => p.Produtos).Where(c => c.CategoriaID <= 5).Take(100).AsNoTracking().ToList();
+            if (categoriasProdutos is null || !categoriasProdutos.Any())
+            {
+                return NotFound();
+            }
+            return categoriasProdutos;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> Get()
+        {
+            var categorias = _context.Categorias.Take(100).AsNoTracking().ToList();
+            if (categorias is null || !categorias.Any())
+            {
+                return NotFound();
+            }
+            return categorias;
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Categoria> Get(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(c => c.CategoriaID == id);
+            if (categoria is null)
+            {
+                return NotFound();
+            }
+            return categoria;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Categoria categoria)
+        {
+            if (categoria is null)
+            {
+                return BadRequest();
+            }
+            _context.Categorias.Add(categoria);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(Get), new { id = categoria.CategoriaID }, categoria);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Categoria categoria)
+        {
+            if (id != categoria.CategoriaID)
+            {
+                return BadRequest();
+            }
+            _context.Entry(categoria).State = EntityState.Modified;
+            _context.SaveChanges();
+            return Ok(categoria);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var categoria = _context.Categorias.Find(id);
+            if (categoria is null)
+            {
+                return NotFound();
+            }
+            _context.Categorias.Remove(categoria);
+            _context.SaveChanges();
+            return Ok(categoria);
+        }
+    }
+}
--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -19,7 +19,7 @@ namespace APICatalogo.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<Produto>> Get()
         {
-            var produtos = _context.Produtos.ToList();
+            var produtos = _context.Produtos.AsNoTracking().ToList();
             if (produtos is null)
             {
                 return NotFound();
@@ -30,7 +30,7 @@ namespace APICatalogo.Controllers
         [HttpGet("{id:int}")]
         public ActionResult<Produto> Get(int id)
         {
-            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoID == id);
+            var produto = _context.Produtos.AsNoTracking().FirstOrDefault(p => p.ProdutoID == id);
             if (produto is null)
             {
                 return NotFound();
