5b5d5646d7da1e3cb619c7f375ff5024fa5b274f
Carlos Bionde <bionde.trab@gmail.com>
2025-07-07 18:56:14 -0300

    feat#40: migrations and start database

---
+++ APICatalogo/Migrations/20250707214703_PopulaProdutos.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Produtos (Nome, Descricao, Preco, ImagemUrl, Estoque, DataCriacao, CategoriaId) values ('Coca-Cola1', 'Refrigerante de cola', 5.00, 'coca-cola1.jpg', 99, now(), 1)");
+            mb.Sql("Insert into Produtos (Nome, Descricao, Preco, ImagemUrl, Estoque, DataCriacao, CategoriaId) values ('Coca-Cola2', 'Refrigerante de cola', 6.00, 'coca-cola2.jpg', 56, now(), 1)");
+            mb.Sql("Insert into Produtos (Nome, Descricao, Preco, ImagemUrl, Estoque, DataCriacao, CategoriaId) values ('Coca-Cola3', 'Refrigerante de cola', 7.00, 'coca-cola3.jpg', 15, now(), 1)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Produtos");
+        }
+    }
+}
