154e7458bb5c3df64dfadda162a51d2f72c96e74
Carlos Bionde <bionde.trab@gmail.com>
2025-07-15 10:11:07 -0300

    alinhando projeto

--- APICatalogo/Context/AppDbContext.cs
+++ APICatalogo/Context/AppDbContext.cs
@@ -1,13 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
-using APICatalogo.Models;
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace APICatalogo.Context;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-    {
+    public AppDbContext(DbContextOptions<AppDbContext> options ) : base( options )
+    {           
     }
-    public DbSet<Produto>? Produtos { get; set; }
     public DbSet<Categoria>? Categorias { get; set; }
+    public DbSet<Produto>? Produtos { get; set; }
 }
--- APICatalogo/DTOs/ProdutoDTO.cs
+++ APICatalogo/DTOs/ProdutoDTO.cs
@@ -1,13 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using APICatalogo.Models;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
-namespace APICatalogo.Models;
+namespace APICatalogo.DTOs;
 
-[Table("Produtos")]
-public class Produto
+public class ProdutoDTO
 {
-    [Key]
     public int ProdutoId { get; set; }
 
     [Required]
@@ -18,15 +17,12 @@ public class Produto
     [StringLength(300)]
     public string? Descricao { get; set; }
     [Required]
-    [Column(TypeName ="decimal(10,2)")]
+
     public decimal Preco { get; set; }
 
     [Required]
     [StringLength(300)]
     public string? ImagemUrl { get; set; }
-    public float Estoque { get; set; }
-    public DateTime DataCadastro { get; set; }
+   
     public int CategoriaId { get; set; }
-    [JsonIgnore]
-    public Categoria? Categoria { get; set; }
 }
